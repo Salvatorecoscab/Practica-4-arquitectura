ce2c827b74d3d72775a952e6172c84a956c455d2
El chavacaba <salvadorcoscab@gmail.com>
2023-06-12 23:47:33 -0600

    added files for p4

---
+++ matrixkeyboard.srcs/sources_1/new/decoderkeys.vhd
@@ -0,0 +1,88 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 06/12/2023 04:17:50 PM
+-- Design Name: 
+-- Module Name: decoderkeys - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity decoderkeys is
+ port(
+    keycode: in std_logic_vector(0 to 15);
+    numEn:  out std_logic_vector(0 to 4)
+ );
+end decoderkeys;
+
+architecture Behavioral of decoderkeys is
+
+begin
+relation:process (keycode) is
+    begin
+    case keycode is
+        when X"0000"=>
+            numEn<="00000";
+        when X"0001"=>
+            numEn<="10000";
+        when X"4000"=>
+            numEn<="10001";
+        when X"2000"=>
+            numEn<="10010";
+        when X"1000"=>
+            numEn<="10011";
+        when X"0800"=>
+            numEn<="10100";
+        when X"0400"=>
+            numEn<="10101";
+        when X"0200"=>
+            numEn<="10110";
+        when X"0100"=>
+            numEn<="10111";
+        when X"0080"=>
+            numEn<="11000"; 
+        when X"0040"=>
+            numEn<="11001";
+        when X"0020"=>
+            numEn<="11010";
+        when X"0010"=>
+            numEn<="11011";   
+        when X"0008"=>
+            numEn<="11100";
+       when X"0004"=>
+            numEn<="11101";
+       when X"0002"=>
+            numEn<="11111";
+       when X"8000"=>
+            numEn<="11110";  
+          
+        when others=>
+            numEn<="00000";
+       
+    end case;
+end process;
+
+end Behavioral;
---
+++ matrixkeyboard.srcs/sources_1/new/stack.vhd
@@ -0,0 +1,83 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 06/12/2023 07:07:22 PM
+-- Design Name: 
+-- Module Name: stack - Behavioral
+-- Project Name: 
+-- Target Devices: 
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+entity stack is port(  Clk:in std_logic; --Clockforthestack.
+                    Reset:in std_logic;--activehighreset.
+                    Enable:in std_logic; --Enablethestack.Otherwiseneitherpushnorpopwillhappen. 
+                    Data_In:in std_logic_vector(15 downto 0); --Datatobepushedtostack
+                    Data_Out:out std_logic_vector(15 downto 0); --Datapoppedfromthestack. 
+                    PUSH_barPOP:in std_logic; --activelowforPOPandactivehighforPUSH. 
+                    Stack_Full:out std_logic; --Goeshighwhenthestackisfull.
+                    Stack_Empty:out std_logic --Goeshighwhenthestackisempty.
+                    );
+end stack;
+architecture Behavioral of stack is 
+type mem_type is array(0 to 255) of std_logic_vector(15 downto 0);
+signal stack_mem:mem_type:=(others=>(others=>'0'));
+signal full,empty:std_logic:='0';
+signal prev_PP: std_logic:='0';
+signal SP:integer:=0; --forsimulationanddebugging.
+begin
+Stack_Full<=full; 
+Stack_Empty<=empty;--PUSHandPOPprocessforthestack. 
+    PUSH:process(Clk,reset)
+    variable stack_ptr:integer:=255;
+    begin
+    if(reset='1') then 
+    stack_ptr:=255; --stackgrowsdownwards. 
+    full<='0';
+    empty<='0';
+    Data_out<=(others=>'0');
+    prev_PP<='0';
+    elsif(rising_edge(Clk)) then--valueofPUSH_barPOPwithoneclockcycledelay.
+    if(Enable='1') then 
+    prev_PP<=PUSH_barPOP;
+    else
+    prev_PP<='0';
+    end if;--POPsection.
+    if(Enable='1' and PUSH_barPOP='0'and empty='0') then--settingemptyflag.
+        if(stack_ptr=255) then 
+        full<='0';
+        empty<='1';
+        else
+        full<='0';
+        empty<='0';
+        end if;--whenthepushbecomespop,beforestackisfull.
+        if(prev_PP='1' and full='0') then 
+        stack_ptr:=stack_ptr+1;
+        end if;--Datahastobetakenfromthenexthighestaddress(emptydescendingtypestack).
+        Data_Out<=stack_mem(stack_ptr);
+        if(stack_ptr/=255) then 
+        stack_ptr:=stack_ptr+1;
+        end if;
+        end if;--PUSHsection.
+        if(Enable='1'and PUSH_barPOP='1'and full='0') then--settingfullflag.
+            if(stack_ptr=0) then 
+            full<='1';
+            empty<='0';
+            else
+            full<='0';
+            empty<='0';
+            end if;--whenthepopbecomespush,beforestackisempty.
+            if(prev_PP='0'and empty='0')then 
+            stack_ptr:=stack_ptr-1;
+            end if;--Datapushedtothecurrentaddress. 
+            stack_mem(stack_ptr)<=Data_In;
+            if(stack_ptr/=0) then 
+            stack_ptr:=stack_ptr-1;
+            end if;
+            end if;
+            SP<=stack_ptr; --fordebugging/simulation.
+            end if;
+            end process;
+            end Behavioral;
